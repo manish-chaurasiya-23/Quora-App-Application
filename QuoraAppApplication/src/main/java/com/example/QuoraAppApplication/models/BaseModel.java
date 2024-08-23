981ef7eb072fd20c5770d0fcce28299c9beb0d93
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-08-23 16:15:27 +0530

    Creating Models for the Entity BaseModel, User, Question, Answer, Comment and Tag and also established RelationShip between them

---
+++ QuoraAppApplication/src/main/java/com/example/QuoraAppApplication/models/BaseModel.java
@@ -0,0 +1,15 @@
+package com.example.QuoraAppApplication.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass // This annotation is used to specify that the class is an entity class that is a super class for other entity classes
+@Data
+public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // This annotation is used to specify the primary key generation strategy to be used
+    private Long id;
+}
