bd124430bf98e9ab500d74b85c59ecfc06e56c2e
Manish Kumar <manishkumar@Manishs-MacBook-Pro.local>
2024-08-27 16:32:28 +0530

    Implemented Jwt token Generation and their method using jjwt Libraray

---
+++ QuoraAppApplication/src/main/java/com/example/QuoraAppApplication/services/JwtService.java
@@ -0,0 +1,69 @@
+package com.example.QuoraAppApplication.services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.expiry}")
+    private int expiry;
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    private String CreateToken(Map<String, Object> payload, String email) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiry*1000L);
+        return Jwts.builder()
+                .claims(payload)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(expiryDate)
+                .subject(email)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey(){
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllPayloads(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllPayloads(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        final String userEmail = extractUsername(token);
+        return userEmail.equals(email) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+}
